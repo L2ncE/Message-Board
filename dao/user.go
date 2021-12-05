e94d5d4789dec69dcba5e61fc95d3c6bdcb185d8
L2ncE <llance_24@foxmail.com>
2021-12-05 15:44:23 +0800

    添加了用户操作接口(查找昵称,更新密码,注册插入)

---
+++ dao/user.go
@@ -0,0 +1,52 @@
+package dao
+
+import (
+	"fmt"
+	"message-board/model"
+)
+
+// UpdatePassword 更新操作
+func UpdatePassword(Name string, newPassword string) error {
+	sqlStr := `update user set Password=? where Name = ?`
+	_, err := dB.Exec(sqlStr, newPassword, Name)
+	if err != nil {
+		fmt.Printf("update failed, err:%v\n", err)
+		return err
+	}
+	return err
+}
+
+// SelectUserByUsername 查找昵称
+func SelectUserByUsername(Name string) (model.User, error) {
+	user := model.User{}
+
+	row := dB.QueryRow("SELECT id, password FROM user WHERE Name = ? ", Name)
+	if row.Err() != nil {
+		return user, row.Err()
+	}
+
+	err := row.Scan(&user.Id, &user.Password)
+	if err != nil {
+		return user, err
+	}
+
+	return user, nil
+}
+
+//insert 插入数据
+func insert(Name string, Password string, Question string, Answer string) {
+
+	sqlStr := "insert into user(name,password,question,answer)values (?,?,?,?)"
+	ret, err := dB.Exec(sqlStr, Name, Password, Question, Answer)
+	if err != nil {
+		fmt.Printf("insert failed, err:%v\n", err)
+		return
+	}
+	// 如果是插入数据的操作，能够拿到插入数据的id
+	id, err := ret.LastInsertId()
+	if err != nil {
+		fmt.Printf("get id failed,err:%v\n", err)
+		return
+	}
+	fmt.Println("id:", id)
+}
