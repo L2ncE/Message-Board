3d9433dd9aa53f071e263d1784eeb99dc5ed6caa
L2ncE <llance_24@foxmail.com>
2021-12-05 17:37:08 +0800

    更新post数据获取

--- dao/post.go
+++ dao/post.go
@@ -3,7 +3,7 @@ package dao
 import "message-board/model"
 
 func InsertPost(post model.Post) error {
-	_, err := dB.Exec("INSERT INTO post(username, txt, post_time, update_time) "+"values(?, ?, ?, ?);", post.Name, post.Context, post.PostTime, post.UpdateTime)
+	_, err := dB.Exec("INSERT INTO post(Name, Context, PostTime, UpdateTime) "+"values(?, ?, ?, ?);", post.Name, post.Context, post.PostTime, post.UpdateTime)
 	return err
 }
 
