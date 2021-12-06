9a4f18d91f8a2671e0ebb778eddcfb1e03d30855
L2ncE <llance_24@foxmail.com>
2021-12-06 20:00:36 +0800

    feat(api):增加了删除留言的接口

--- api/router.go
+++ api/router.go
@@ -17,8 +17,9 @@ func InitEngine() {
 	postGroup := engine.Group("/post")
 	{
 		postGroup.Use(auth)
-		postGroup.POST("/", addPost) //发布新留言
-		postGroup.POST("/:post_id")  //修改留言
+		postGroup.POST("/", addPost)  //发布新留言
+		postGroup.POST("/:post_id")   //修改留言
+		postGroup.DELETE("/:post_id") //删除留言
 
 		postGroup.GET("/", briefPosts)         //查看全部留言概略
 		postGroup.GET("/:post_id", postDetail) //查看一条留言详细信息和其下属评论
