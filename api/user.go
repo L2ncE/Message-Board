af308d74490b384e3d76125288a8b351cfe20040
L2ncE <llance_24@foxmail.com>
2021-12-08 21:21:36 +0800

    feat(api):增加注册时对密码和用户名长度的检验

--- api/user.go
+++ api/user.go
@@ -47,42 +47,37 @@ func changePassword(ctx *gin.Context) {
 func login(ctx *gin.Context) {
 	username := ctx.PostForm("username")
 	password := ctx.PostForm("password")
-	l1 := len([]rune(password))
-	if l1 <= 16 {
-		flag, err := service.IsPasswordCorrect(username, password)
-		if err != nil {
-			fmt.Println("judge password correct err: ", err)
-			tool.RespInternalError(ctx)
-			return
-		}
 
-		if !flag {
-			tool.RespErrorWithDate(ctx, "登陆失败,请输入密保")
-			answer := ctx.PostForm("answer")
-			if answer == service.SelectAnswerByUsername(username) {
-				ctx.JSON(200, gin.H{
-					"msg": "密保正确,请重新输入密码",
-				})
-				newPassword := ctx.PostForm("new_password")
-				err = service.ChangePassword(username, newPassword)
-				if err != nil {
-					fmt.Println("change password err: ", err)
-					tool.RespInternalError(ctx)
-					return
-				}
-			} else {
-				tool.RespErrorWithDate(ctx, "密保错误")
-				return
-			}
-			return
-		}
-
-		ctx.SetCookie("username", username, 600, "/", "", false, false)
-		tool.RespSuccessful(ctx)
-	} else {
-		tool.RespErrorWithDate(ctx, "密码请在16位之内")
+	flag, err := service.IsPasswordCorrect(username, password)
+	if err != nil {
+		fmt.Println("judge password correct err: ", err)
+		tool.RespInternalError(ctx)
 		return
 	}
+
+	if !flag {
+		tool.RespErrorWithDate(ctx, "登陆失败,请输入密保")
+		answer := ctx.PostForm("answer")
+		if answer == service.SelectAnswerByUsername(username) {
+			ctx.JSON(200, gin.H{
+				"msg": "密保正确,请重新输入密码",
+			})
+			newPassword := ctx.PostForm("new_password")
+			err = service.ChangePassword(username, newPassword)
+			if err != nil {
+				fmt.Println("change password err: ", err)
+				tool.RespInternalError(ctx)
+				return
+			}
+		} else {
+			tool.RespErrorWithDate(ctx, "密保错误")
+			return
+		}
+		return
+	}
+
+	ctx.SetCookie("username", username, 600, "/", "", false, false)
+	tool.RespSuccessful(ctx)
 }
 
 func register(ctx *gin.Context) {
@@ -90,32 +85,43 @@ func register(ctx *gin.Context) {
 	password := ctx.PostForm("password")
 	question := ctx.PostForm("question")
 	answer := ctx.PostForm("answer")
+	l1 := len([]rune(username))
+	l2 := len([]rune(password))
+	if l1 <= 8 {
+		if l2 <= 16 {
+			user := model.User{
+				Name:     username,
+				Password: password,
+				Question: question,
+				Answer:   answer,
+			}
 
-	user := model.User{
-		Name:     username,
-		Password: password,
-		Question: question,
-		Answer:   answer,
-	}
+			flag, err := service.IsRepeatUsername(username)
+			if err != nil {
+				fmt.Println("judge repeat username err: ", err)
+				tool.RespInternalError(ctx)
+				return
+			}
 
-	flag, err := service.IsRepeatUsername(username)
-	if err != nil {
-		fmt.Println("judge repeat username err: ", err)
-		tool.RespInternalError(ctx)
+			if flag {
+				tool.RespErrorWithDate(ctx, "用户名已经存在")
+				return
+			}
+
+			err = service.Register(user)
+			if err != nil {
+				fmt.Println("register err: ", err)
+				tool.RespInternalError(ctx)
+				return
+			}
+
+			tool.RespSuccessful(ctx)
+		} else {
+			tool.RespErrorWithDate(ctx, "用户名请在8位之内")
+			return
+		}
+	} else {
+		tool.RespErrorWithDate(ctx, "密码请在16位之内")
 		return
 	}
-
-	if flag {
-		tool.RespErrorWithDate(ctx, "用户名已经存在")
-		return
-	}
-
-	err = service.Register(user)
-	if err != nil {
-		fmt.Println("register err: ", err)
-		tool.RespInternalError(ctx)
-		return
-	}
-
-	tool.RespSuccessful(ctx)
 }
