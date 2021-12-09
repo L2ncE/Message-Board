b78ce83a6162b364cbc1dab26defb4b8342a7226
L2ncE <llance_24@foxmail.com>
2021-12-09 10:08:03 +0800

    feat(api):增加管理员中间件

--- api/middleware.go
+++ api/middleware.go
@@ -15,3 +15,15 @@ func auth(ctx *gin.Context) {
 	ctx.Set("username", username)
 	ctx.Next()
 }
+
+func admin(ctx *gin.Context) {
+	if cookie, err := ctx.Cookie("admin"); err == nil {
+		if cookie == "袁鑫浩" {
+			ctx.Next()
+		}
+	}
+	// 返回错误
+	tool.RespErrorWithDate(ctx, "你好,你不是管理员")
+	ctx.Abort()
+	return
+}
