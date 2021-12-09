8b3402efffac8f590e84a335c54774f59c06516f
L2ncE <llance_24@foxmail.com>
2021-12-09 10:09:20 +0800

    refactor(api):增加管理员直接删除留言与评论

--- api/router.go
+++ api/router.go
@@ -18,27 +18,39 @@ func InitEngine() {
 
 	postGroup := engine.Group("/post")
 	{
-		postGroup.Use(auth)
-		postGroup.POST("/", addPost)              //发布新留言
-		postGroup.POST("/:post_id", changePost)   //修改留言
-		postGroup.DELETE("/:post_id", deletePost) //删除留言
+		{
+			postGroup.Use(auth)
+			postGroup.POST("/", addPost)              //发布新留言
+			postGroup.POST("/:post_id", changePost)   //修改留言
+			postGroup.DELETE("/:post_id", deletePost) //删除留言
 
-		postGroup.GET("/", briefPosts)         //查看全部留言概略
-		postGroup.GET("/:post_id", postDetail) //查看一条留言详细信息和其下属评论
+			postGroup.GET("/", briefPosts)         //查看全部留言概略
+			postGroup.GET("/:post_id", postDetail) //查看一条留言详细信息和其下属评论
 
-		engine.MaxMultipartMemory = 8 << 20
-		postGroup.POST("/file", uploadFile) ////上传文章功能
+			engine.MaxMultipartMemory = 8 << 20
+			postGroup.POST("/file", uploadFile) ////上传文章功能
 
-		postGroup.POST("/:post_id/likes", postLikes)
+			postGroup.POST("/:post_id/likes", postLikes)
+		}
+		{
+			postGroup.Use(admin)
+			postGroup.DELETE("/:post_id0", deletePost0) //管理员删除,无视用户名
+		}
 	}
 
 	commentGroup := engine.Group("/comment")
 	{
-		commentGroup.Use(auth)
-		commentGroup.POST("/", addComment)                 //发送评论
-		commentGroup.DELETE("/:comment_id", deleteComment) //删除评论
+		{
+			commentGroup.Use(auth)
+			commentGroup.POST("/", addComment)                 //发送评论
+			commentGroup.DELETE("/:comment_id", deleteComment) //删除评论
 
-		commentGroup.POST("/:comment_id/likes", commentLikes)
+			commentGroup.POST("/:comment_id/likes", commentLikes)
+		}
+		{
+			commentGroup.Use(admin)
+			commentGroup.DELETE("/:comment_id0", deleteComment0) //管理员删除,无视用户名
+		}
 	}
 
 	err := engine.Run()
